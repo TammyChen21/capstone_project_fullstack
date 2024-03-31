0a45a256aa5c64e77de60158868c6c1a0581edb0
siqichen <siqi1987@hotmail.com>
2024-04-01 00:11:23 +0200

    Add useEffect function for CheckButton in frontend to management the status with time

--- frontend/src/components/CheckButton.tsx
+++ frontend/src/components/CheckButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 type CheckButtonProps = {
     onCheck: () => void;
@@ -7,6 +7,23 @@ type CheckButtonProps = {
 
 export default function CheckButton({ onCheck, onUncheck }: CheckButtonProps) {
     const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const now = new Date();
+            if (now.getHours() === 0 && now.getMinutes() === 0) {
+                if (checked) {
+                    onUncheck();
+                    setChecked(false);
+                } else {
+                    onCheck();
+                    setChecked(true);
+                }
+            }
+        }, 60000);
+
+        return () => clearInterval(intervalId);
+    }, [checked, onCheck, onUncheck]);
 
     const handleClick = () => {
         if (checked) {
