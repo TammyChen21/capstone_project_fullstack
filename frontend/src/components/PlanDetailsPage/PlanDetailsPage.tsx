134bd85061ac0783bcd4f07addae721b3e671009
siqichen <siqi1987@hotmail.com>
2024-04-07 14:37:10 +0200

    Add fetch function for calendar component

--- frontend/src/components/PlanDetailsPage/PlanDetailsPage.tsx
+++ frontend/src/components/PlanDetailsPage/PlanDetailsPage.tsx
@@ -1,9 +1,47 @@
 import Calendar from "react-calendar";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {Plan} from "../../types/Plan.ts";
 
+type plans={
+    id: string;
+    description: string;
+    checked: boolean;
+    datumOfCheckIns: Date;
+    numberOfCheckIns: number;
+
+}
 export default function PlanDetailsPage(): JSX.Element {
+    const [plans, setPlans] = useState<Plan[]>([]);
+
+    useEffect(() => {
+        fetchPlans();
+    }, []);
+
+    const fetchPlans = () => {
+        axios.get('/api/plan')
+            .then(response => {
+                setPlans(response.data);
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.error('Error fetching plans:', error);
+            });
+    };
+
+    const tileContent = ({ date, view }: { date: Date; view: string }) => {
+        if (view === 'month') {
+            const plan = plans.find(plan => new Date(plan.datumOfCheckIns).toDateString() === date.toDateString());
+            if (plan) {
+                return plan.checked ? '✔️' : '❌';
+            }
+        }
+        return null;
+    };
+
     return (
         <div>
-            <Calendar />
+            <Calendar tileContent={tileContent}  />
         </div>
     )
 }
