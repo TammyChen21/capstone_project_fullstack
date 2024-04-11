92fd4a78dbbe50e84fb3639e8ad34dc60598f171
siqichen <siqi1987@hotmail.com>
2024-04-11 12:57:35 +0200

    fixed problems from SonarLint

--- backend/src/test/java/de/neuefische/backend/controller/PlanListControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/PlanListControllerTest.java
@@ -11,20 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
-import java.util.Optional;
-
-import static com.mongodb.internal.connection.tlschannel.util.Util.assertTrue;
-import static org.bson.assertions.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
